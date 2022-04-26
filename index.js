7fd209d7b48de990b63a09e587b74ef00a4082d7
root <yluo3421@gmail.com>
2022-04-26 13:05:36 -0400

    commit tree 2637e83e1d41cf18473bdbaf47348dcca8ad6947

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "d";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "f";
+}
